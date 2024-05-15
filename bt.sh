8195cc40f1cea51aea4db95ddb179edaf7a66870
lesfox <addresseui@gmail.com>
2024-05-15 15:00:54 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -8,6 +8,9 @@ trap 'rm -f "${TMPFILE}"' EXIT
 TMPFILE=$(mktemp)
 ## 获取列表内容并合并,可按照格式自行添加列表
 curl -k {https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt,\
+https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best.txt,\
+https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/http.txt,\
+https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/nohttp.txt,\
 http://github.itzmx.com/1265578519/OpenTracker/master/tracker.txt,\
 https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt,\
 http://t.acg.rip:6699/announce,\
