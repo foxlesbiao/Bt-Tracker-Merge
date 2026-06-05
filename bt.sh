bb0b4f71d04291981e0bcc58b8650c2a74726557
foxlesbiao <foxlesbiao@users.noreply.github.com>
2026-06-06 02:02:37 +0800

    cleanup: remove dead tracker sources (t.acg.rip, yaozuopan.top), fix trackerslist.com URL typo, simplify grep escaping

--- bt.sh
+++ bt.sh
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 mkdir tracker
 cd tracker
 ## 列表文件名
@@ -7,6 +7,8 @@ LIST="tracker.txt"
 trap 'rm -f "${TMPFILE}"' EXIT
 TMPFILE=$(mktemp)
 ## 获取列表内容并合并,可按照格式自行添加列表
+## 活跃源：XIU2(每日更新), ngosang(每日更新), itzmx, newtrackon, 迅雷, trackerslist.com, DeSireFire(停更但仍可访问)
+## 已移除：t.acg.rip(连接失败), yaozuopan.top(站点挂了)
 curl -k {https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt,\
 https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/best.txt,\
 https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/http.txt,\
@@ -20,7 +22,6 @@ https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ws.tx
 https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt,\
 https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt,\
 https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt,\
-http://t.acg.rip:6699/announce,\
 https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt,\
 https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all_udp.txt,\
 https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all_https.txt,\
@@ -30,9 +31,8 @@ https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all_ip.t
 https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_best.txt,\
 https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_best_ip.txt,\
 https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_bad.txt,\
-https:trackerslist.com/all.txt,\
-https://www.yaozuopan.top/dynamic/trackerlist.txt,\
+https://trackerslist.com/all.txt,\
 https://newtrackon.com/api/live,\
 http://plugin.xl7.xunlei.com/7.9/normal/trackers.json} > ${TMPFILE}
 ## 对列表排序并去重
-grep -Po '(udp|https?|wss?):\/\/.+?\/announce(\.php)?' ${TMPFILE} | sort -u -o ${LIST}
+grep -Po '(udp|https?|wss?)://.+?/announce(.php)?' ${TMPFILE} | sort -u -o ${LIST}
